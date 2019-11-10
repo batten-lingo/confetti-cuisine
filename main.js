ccbad6192921a5b0b25c205978214d502814e2c0
batten-lingo <batten_ringo@hotmail.com>
2019-11-10 22:00:25 +0900

    LESSON7まで(ソースコードのみ)

--- main.js
+++ main.js
@@ -6,4 +6,58 @@ const port = 3000,
     router = require("./router"),
     contentTypes = require("./contentTypes"),
     utils = require("./utils");
-    
+
+// Web ページとアセットについて、一連の経路を追加
+router.get("/", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/index.html", res);
+});
+
+router.get("/courses.html", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/courses.html", res);
+});
+
+router.get("/contact.html", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/contact.html", res);
+});
+
+router.post("/", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/index.html", res);
+});
+
+router.get("/graph.png", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.png);
+    utils.getFile("public/images/graph.png", res);
+});
+
+router.get("/people.jpg", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/images/people.jpg", res);
+});
+
+router.get("/product.jpg", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/images/people.jpg", res);
+});
+
+router.get("/confetti-cuisine.css", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.css);
+    utils.getFile("public/css/confetti-cuisine.css", res);
+});
+
+router.get("/bootstrap.css", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.css);
+    utils.getFile("public/css/bootstrap.css", res);
+});
+
+router.get("/confetti-cuisine.js", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.js);
+    utils.getFile("public/js/confetti-cuisine.js", res);
+});
+
+// サーバーを起動
+http.createServer(router.handle).listen(port);
+console.log(`The server is listening on port number: ${port}`);
--- utils.js
+++ utils.js
@@ -8,7 +8,7 @@ module.exports = {
     getFile: (file, res) => {
         fs.readFile(`./${file}`, (error, data) => {
             if (error) {
-                res.wrieHead(httpStatus.INTERNAL_SERVER_ERROR, contentTypes.html);
+                res.writeHead(httpStatus.INTERNAL_SERVER_ERROR, contentTypes.html);
                 res.end("There was an error serving content!");
             }
             res.end(data);
